87953e192c55a999c3de257f87cf06c2e011971f
jesusdarias <71895708+jesusdarias@users.noreply.github.com>
2021-11-26 14:21:13 -0500

    Add files via upload

---
+++ Practica2.3/ejercicio7_exec.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char **argv){
+
+
+	if (argc < 2) {
+   	   printf("Usage: %s <command> <options>\n",argv[0]);
+   	   return -1;
+      	}
+	pid_t pid = fork();
+
+	if(pid==-1)
+	{ 
+		perror("Error al hacer fork");
+		exit(1);
+	}
+	else if(pid==0)
+	{
+		//hijo
+		execvp(argv[1], argv+1);
+	}
+	else
+	{
+		//padre
+		int status;
+		wait(&status);
+		
+		if(status==-1)
+		{
+			perror("El proceso hijo no terminó correctamente.");
+			exit(1);
+		}
+
+		printf("El comando terminó de ejecutarse\n");
+
+	}
+
+	return 0;
+
+}
+
